91b34e22656b7f8533f0176b83bd3d6ab3b27ca6
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2024-10-22 07:44:12 +0000

    kattis: add hipphipphurra.cpp

---
+++ kattis/hipphipphurra.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int x;
+    cin >> x;
+
+    while (x--) {
+        cout << "Hipp hipp hurra, " << s << '!' << '\n';
+    }
+}
