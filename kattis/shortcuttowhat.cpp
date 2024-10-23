c14b940f8ef97ef1204c2c9115b22f40466729da
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2024-10-23 08:25:12 +0300

    kattis: leynithjonusta shortcuttowhat takkfyrirmig

---
+++ kattis/shortcuttowhat.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    n += 5;
+    n *= 3;
+    n -= 10;
+
+    cout << n << endl;
+}
---
+++ kattis/takkfyrirmig.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n--) {
+        string s;
+        cin >> s;
+        cout << "Takk " << s << endl;
+    }
+}
