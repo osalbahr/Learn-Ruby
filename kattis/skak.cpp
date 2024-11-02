e0ed9d2d1e45c674fb5307c922a892884a5aef01
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2024-11-02 10:55:25 +0300

    kattis: add skak.cpp

---
+++ kattis/skak.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int xh, yh, xp, yp;
+    cin >> xh >> yh >> xp >> yp;
+
+    if (xh == xp || yh == yp)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << 2 << endl;
+    }
+}
