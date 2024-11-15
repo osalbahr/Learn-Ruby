53bba760a786a8f1377b4291faf267c26c1c3825
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2024-11-15 14:04:03 +0300

    euler: add problem 1

---
+++ euler/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int total = 0;
+
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            total += i;
+        }
+    }
+
+    cout << total << endl;
+}
