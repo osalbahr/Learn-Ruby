6d18419a8d71ada1e653a1bab1a627d823c7e040
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2024-11-20 16:02:43 +0300

    cf: add 1996A

---
+++ codeforces/1996/a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    cout << (n + 3) / 4 << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
