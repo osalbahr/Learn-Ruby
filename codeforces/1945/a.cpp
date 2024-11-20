993eff3a90ec118a278ee39a7fe1bf5ca8bf8c5f
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2024-11-20 22:18:08 +0300

    cf: add 1945A partial solution

---
+++ codeforces/1945/a.cpp
@@ -0,0 +1,23 @@
+// Note: partial solution
+
+#include <iostream>
+
+using namespace std;
+
+int solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--) {
+        cout << solve() << endl;
+    }
+}
