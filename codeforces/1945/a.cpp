25d2b5882cc8e75a8d2c0e55c223818a6ee4b9db
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2024-11-20 22:35:13 +0300

    cf: complete 1945A

--- codeforces/1945/a.cpp
+++ codeforces/1945/a.cpp
@@ -4,12 +4,32 @@
 
 using namespace std;
 
+#define REPORT(X) cout << #X << " = " << (X) << endl
+
 int solve()
 {
     int a, b, c;
     cin >> a >> b >> c;
 
+    int total = a;
 
+    if (b % 3 == 0) {
+        total += b / 3;
+        total += (c + 2) / 3;
+        return total;
+    }
+
+    // b is not multiple of 3
+    int rem = 3 - (b % 3);
+    if (c < rem)
+        return -1;
+    
+    b += rem;
+    c -= rem;
+
+    total += b / 3;
+    total += (c + 2) / 3;
+    return total;
 }
 
 int main()
