b8d6432dbd109a877ab781e9549a823942e13182
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2024-11-27 19:40:12 +0300

    aoc: add 2015 day 6

---
+++ adventofcode/2015/day06.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    vector<vector<bool>> grid(1000);
+    for (auto& row : grid) {
+        row.resize(1000);
+    }
+
+    string line;
+    while (getline(cin, line)) {
+        stringstream ss(line);
+
+        string command;
+        ss >> command;
+
+        // turn on/off
+        if (command == "turn") {
+            ss >> command;
+        }
+
+        int x1, y1;
+        char comma;
+        ss >> x1 >> comma >> y1;
+
+        string through;
+        ss >> through;
+
+        int x2, y2;
+        ss >> x2 >> comma >> y2;
+
+        if (command == "toggle") {
+            for (int i = x1; i <= x2; i++) {
+                for (int j = y1; j <= y2; j++) {
+                    grid[i][j] = !grid[i][j];
+                }
+            }
+        } else if (command == "on") {
+            for (int i = x1; i <= x2; i++) {
+                for (int j = y1; j <= y2; j++) {
+                    grid[i][j] = true;
+                }
+            }
+        } else { // command == "off"
+            for (int i = x1; i <= x2; i++) {
+                for (int j = y1; j <= y2; j++) {
+                    grid[i][j] = false;
+                }
+            }
+        }
+    }
+
+    int total = 0;
+    for (int i = 0; i < 1000; i++) {
+        for (int j = 0; j < 1000; j++) {
+            total += grid[i][j];
+        }
+    }
+
+    cout << total << endl;
+}
