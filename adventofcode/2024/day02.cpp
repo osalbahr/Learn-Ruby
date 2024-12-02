6a30005167289210f621f103b8941a42b56076ca
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2024-12-02 08:16:33 +0300

    aoc: add 2024 day 2 part 1

---
+++ adventofcode/2024/day02.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+bool isSafe(string report)
+{
+    stringstream ss(report);
+
+    vector<int> levels;
+    
+    int level;
+    while (ss >> level)
+        levels.push_back(level);
+    
+    bool decreasing = true;
+    bool increasing = true;
+    for (int i = 0; i < levels.size() - 1; i++) {
+        if (abs(levels[i] == levels[i + 1])) {
+            return false;
+        }
+
+        if (abs(levels[i] - levels[i + 1]) > 3) {
+            return false;
+        }
+
+        if (levels[i + 1] > levels[i]) {
+            decreasing = false;
+        }
+
+        if (levels[i + 1] < levels[i]) {
+            increasing = false;
+        }
+    }
+
+    return decreasing || increasing;
+}
+
+int main()
+{
+    int total = 0;
+
+    string report;
+    while (getline(cin, report)) {
+        total += isSafe(report);
+    }
+
+    cout << total << endl;
+}
